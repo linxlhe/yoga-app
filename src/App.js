0731005b8a44b77985fd97886b8757c43c2b4c74
Lin He <xlhe@cs.washington.edu>
2024-11-21 12:27:35 -0800

    zach's version

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { DndContext, closestCorners } from "@dnd-kit/core";
 import { initialPoses } from "./data/initialPoses";
 import PoseList2 from "./components/PoseList2";
@@ -7,64 +7,106 @@ import PoseList1 from "./components/PoseList1";
 import { arrayMove } from "@dnd-kit/sortable";
 import { Input } from "./components/Input";
 
+/**
+ * interface PoseTemplate {
+ *   id: number; // needed by dnd-context
+ *   name: string; 
+ * }
+ * 
+ * interface PoseInstance {
+ *   id: number;
+ *   
+ *   templateId: number;
+ *   name: string;
+ * }
+ * 
+ * type ManagedListItem = 
+ *  | { kind: "template", value: PoseTemplate }
+ *  | { kind: "instance", value: PoseInstance }
+ * 
+ *
+ */
+
+const startingList = initialPoses.map((template) => ({ kind: "template", value: template }));
+
 function App() {
-  const [sequence, setSequence] = useState([]);
-  const [poses, setPoses] = useState(initialPoses);
+  const counter = useRef(startingList.length);
+  const getId = () => {
+    const value = counter.value;
+    counter.value++;
 
-  const handleDragEndSequence = (event) => {
+    return value;
+  };
+
+  const [state, setState] = useState(startingList);
+
+
+  const templates = state.filter(({ kind }) => kind === "template").map(({ value }) => value);
+  const sequence = state.filter(({ kind }) => kind === "instances").map(({ value }) => value);
+
+  console.log(templates);
+
+  const lookup = new Map(state.map((value) => [value.value.id, value]));
+
+  const handleDragEnd = (event) => {
+    console.log(event);
     const { active, over } = event;
     if (!over || active.id === over.id) return;
 
-    setSequence((currentSequence) => {
-      const originalPos = currentSequence.findIndex(
-        (pose) => pose.id === active.id
-      );
-      const newPos = currentSequence.findIndex((pose) => pose.id === over.id);
-      return arrayMove(currentSequence, originalPos, newPos);
-    });
-  };
+    const dragItem = lookup.get(active.id);
+    const dropItem = lookup.get(over.id);
 
-  const handleDragEndPoses = (event) => {
-    const { active, over } = event;
-    if (!over || active.id === over.id) return;
+    if (!dragItem || !dropItem) {
+      console.warn("How did this get undefined????");
+      return;
+    }
 
-    setPoses((currentPoses) => {
-      const originalPos = currentPoses.findIndex(
-        (pose) => pose.id === active.id
-      );
-      const newPos = currentPoses.findIndex((pose) => pose.id === over.id);
-      return arrayMove(currentPoses, originalPos, newPos);
-    });
-  };
+    console.log(dragItem, dropItem);
 
-  const addPoseToSequence = (name) => {
-    setSequence(poses => [...poses, {id: poses.length+1, name }])
-  };
+    if (dragItem.kind === dropItem.kind) {
+      setState((state) => {
+        const dragIndex = state.findIndex(({ value: { id } }) => id === dragItem.value.id);
+        const dropIndex = state.findIndex(({ value: { id } }) => id === dropItem.value.id);
+
+        return arrayMove(state, dragIndex, dropIndex)
+      });
+    } else if (dragItem.kind === "template") {
+      const newElement = {
+        kind: "instance",
+        value: {
+          id: getId(),
+          templateId: dragItem.value.id,
+          name: dragItem.value.name
+        }
+      }
+
+      setState((state) => [...state, newElement]);
+    } else {
+      setState((state) => {
+        return state.filter(({ value: { id } }) => id !== dragItem.value.id);
+      })
+    }
+  }
 
   const addPoseToPoseList = (name) => {
-    setPoses(poses => [...poses, {id: poses.length+1, name }])
+    setState((state) => [...state, { kind: "template", value: {id: getId(), name } }]);
   };
 
   return (
     <>
       <DndContext
-        onDragEnd={handleDragEndPoses}
+        onDragEnd={handleDragEnd}
         collisionDetection={closestCorners}
       >
         <div>
           <h1>Postures</h1>
           <Input onSubmit={addPoseToPoseList} />
-          <PoseList1 poses={poses} />
+          <PoseList1 poses={templates} />
         </div>
-      </DndContext>
 
-      <DndContext
-        onDragEnd={handleDragEndSequence}
-        collisionDetection={closestCorners}
-      >
         <div>
           <h1>My Sequence</h1>
-          <Input onSubmit={addPoseToSequence} />
+          {/* <Input onSubmit={addPoseToSequence} /> */}
           <PoseList2 poses={sequence} />
         </div>
       </DndContext>
